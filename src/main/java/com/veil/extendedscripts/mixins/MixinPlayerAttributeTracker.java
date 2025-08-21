37fd67296dad07bec0085630e372f9259af07ddd
Veil <loomingveil@gmail.com>
2025-08-21 16:24:54 -0400

    Update MixinPlayerAttributeTracker.java

--- src/main/java/com/veil/extendedscripts/mixins/MixinPlayerAttributeTracker.java
+++ src/main/java/com/veil/extendedscripts/mixins/MixinPlayerAttributeTracker.java
@@ -6,6 +6,7 @@ import kamkeel.npcs.controllers.data.attribute.tracker.PlayerAttributeTracker;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 
 @Mixin(value={PlayerAttributeTracker.class})
 public class MixinPlayerAttributeTracker {
+    @Unique
     EntityPlayer player;
 
     @Inject(
